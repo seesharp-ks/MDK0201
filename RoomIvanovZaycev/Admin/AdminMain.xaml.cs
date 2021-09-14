f87f1bbabbf42f073a551a746f6230c80096016c
seesharp-ks <90371154+seesharp-ks@users.noreply.github.com>
2021-09-14 14:47:46 +0300

    init

---
+++ RoomIvanovZaycev/Admin/AdminMain.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoomIvanovZaycev.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminMain.xaml
+    /// </summary>
+    public partial class AdminMain : Window
+    {
+        public AdminMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListExitProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnListCloseAPP_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
